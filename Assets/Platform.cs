42765ea09237873888d665c9e08c7f5033b333d7
DESKTOP-7PC7I35\andre <andreasforzi01@gmail.com>
2023-04-18 12:13:18 +0200

    piattaforma

--- Assets/Platform.cs
+++ Assets/Platform.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if(collision.CompareTag("Player"))
+        collision.transform.SetParent(transform);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D collision)
     {
-        
+        if (collision.CompareTag("Player"))
+            collision.transform.SetParent(null);
     }
 }
